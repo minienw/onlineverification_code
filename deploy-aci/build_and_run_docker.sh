b6d79b0997a3e4543cc188220fbfb5271421eea5
playtime222 <steve.kellaway@mefitihe.com>
2022-05-04 19:38:50 +0200

    Updated container for K8S in Azure.

--- deploy-aci/build_and_run_docker.sh
+++ deploy-aci/build_and_run_docker.sh
@@ -1,2 +1,4 @@
-docker build -t validation_service:aci .
-docker-compose -p validation up
+docker build -t validation_service:aks_latest .
+#docker-compose -p validation up
+docker tag validation_service:aks_latest stevekellaway/validation_service:aks_latest
+docker push stevekellaway/validation_service:aks_latest
