2bfbefa50ede7ec9c45294c837027971b7cdf66c
playtime222 <steve.kellaway@mefitihe.com>
2022-05-05 15:33:50 +0200

    Seperate folder for building image for K8S

--- deploy-azk/build_and_push_docker.sh
+++ deploy-azk/build_and_push_docker.sh
@@ -1,4 +1,3 @@
 docker build -t validation_service:aks_latest .
-#docker-compose -p validation up
 docker tag validation_service:aks_latest stevekellaway/validation_service:aks_latest
 docker push stevekellaway/validation_service:aks_latest
