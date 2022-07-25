465845e8aa46246eeab7925c41ee081e9f8c3dcb
playtime222 <steve.kellaway@mefitihe.com>
2022-07-25 14:29:00 +0200

    Update docker repo to use for K8S deployment

--- deploy-azk/build_and_push_docker.sh
+++ deploy-azk/build_and_push_docker.sh
@@ -1,3 +1,3 @@
 docker build -t validation_service:azk_latest .
-docker tag validation_service:azk_latest stevekellaway/validation_service:azk_latest
-docker push stevekellaway/validation_service:azk_latest
+docker tag validation_service:azk_latest ghcr.io/minienw/validation_service:azk_latest
+docker push ghcr.io/minienw/validation_service:azk_latest
